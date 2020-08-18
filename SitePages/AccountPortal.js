8851c2a3dc57d14a4972a3797e125de34b336989
WideLuke <52456731+WideLuke@users.noreply.github.com>
2020-08-18 21:09:09 +0100

    File moving
    Moved files from document folder to repository.

---
+++ SitePages/AccountPortal.js
@@ -0,0 +1,62 @@
+function checkPin(){
+  if(document.getElementById('pin').value == '1010' && document.getElementById('username').value == 'Admin'){
+  let userName = document.getElementById('username').value;
+  alert('Correct Info! Welcome, ' + userName); 
+  location.href="accountportal.html";
+  } else {
+  alert('Wrong Info!');
+  return false;
+  }}
+
+function HideAllElements()
+{
+  document.getElementById('CreateNotes').style.display = "none";
+  document.getElementById('CreateMessages').style.display = "none";
+}
+
+window.onload = HideAllElements();
+
+function ToggleDiv(Div) {
+  HideAllElements();
+  var x = document.getElementById(Div)
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function download(filename, text) {
+  var pom = document.createElement('a');
+  pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + 
+
+encodeURIComponent(text));
+  pom.setAttribute('download', filename);
+
+  pom.style.display = 'none';
+  document.body.appendChild(pom);
+
+  pom.click();
+
+  document.body.removeChild(pom);
+}
+
+function addTextHTML()
+{
+    document.addtext.name.value = document.addtext.name.value + ".html"
+}
+
+function addTextTXT()
+{
+    document.addtext.name.value = document.addtext.name.value + ".txt"
+}
+
+function sendMessage(){
+  if(document.getElementById('MessageContent').value == 'What is this?'){
+   alert('This is your admin portal.'); 
+   } else {
+     alert('Invalid');
+   }
+  }
+
+
